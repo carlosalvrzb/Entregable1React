a76572b9eb65d67b3c1d0ebc9ffabb628a890b05
Carlos Alvarez <carlosalvrzb@gmail.com>
2022-11-23 21:00:01 -0600

    entregable

---
+++ entregable1-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
+
+export default App
